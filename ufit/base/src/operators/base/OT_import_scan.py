298833f83464c75532c95609b695ed7511dd6950
bertjanssens1993 <bertjanssens93@gmail.com>
2023-11-30 11:26:48 +0100

    dynamically change the filter_glob

--- ufit/base/src/operators/base/OT_import_scan.py
+++ ufit/base/src/operators/base/OT_import_scan.py
@@ -5,7 +5,7 @@ from .....base.src.operators.core.start import init_modeling_folders, import_3d_
 
 
 class OTImportScan(OTBase, ImportHelper):
-
+    filename_ext = ''
     filter_glob: bpy.props.StringProperty(
         default='*.zip;*.stl;*.obj',
         options={'HIDDEN'}
@@ -15,6 +15,18 @@ class OTImportScan(OTBase, ImportHelper):
     def poll(cls, context):
         # always make it possible to import a scan
         return True
+
+    def invoke(self, context, event):
+        if context.scene.ufit_file_type == 'zip':
+            self.filter_glob = '*.zip;'
+        elif context.scene.ufit_file_type == 'stl':
+            self.filter_glob = '*.stl;'
+        elif context.scene.ufit_file_type == 'obj':
+            self.filter_glob = '*.obj;'
+        else:
+            self.filter_glob = '*.zip;*.stl;*.obj'
+
+        return super().invoke(context, event)
 
     def main_func(self, context):
 
